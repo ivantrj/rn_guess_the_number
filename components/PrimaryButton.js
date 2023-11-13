7a699dd4980bf5d15eb797fcc01cc2106306b457
ivan <ivantrajanovski@gmail.com>
2023-11-13 16:22:26 +0100

    improve buttons

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,10 +1,34 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 import React from "react";
 
 export default function PrimaryButton({ children }) {
   return (
-    <View>
-      <Text>{children}</Text>
-    </View>
+    <Pressable
+      style={({ pressed }) => [pressed && { opacity: 0.7 }]}
+      android_ripple={{ color: "#ddb52f" }}
+    >
+      <View style={styles.buttonContainer}>
+        <Text style={styles.buttonText}>{children}</Text>
+      </View>
+    </Pressable>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    backgroundColor: "#ddb52f",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    margin: 4,
+    borderRadius: 28,
+    elevation: 3,
+    shadowColor: "black",
+    shadowOffset: { width: 1, height: 1 },
+    shadowRadius: 2,
+    shadowOpacity: 0.4,
+  },
+  buttonText: {
+    // color: "white",
+    textAlign: "center",
+  },
+});
